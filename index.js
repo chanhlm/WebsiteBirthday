04f99daf989508599edd8db01e6f8f10cd589ada
chanhlm <minhchanh.lmc@gmail.com>
2024-02-02 23:09:00 +0700

    push: finish code

--- index.js
+++ index.js
@@ -299,7 +299,7 @@ function reveal() {
   video.setAttribute("autoplay", "autoplay");
   video.setAttribute("width", "300");
   video.setAttribute("height", "auto");
-  
+  wideo.setAttribute("style", "padding-top: 40px;");
   document.querySelector('#video').appendChild(video);
 }
 
